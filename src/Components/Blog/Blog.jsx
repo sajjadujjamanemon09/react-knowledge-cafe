4a4e0530f1aec5f04f978ae34a63eb86408d0cdf
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-09-11 20:22:43 +0600

    second commit

---
+++ src/Components/Blog/Blog.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+const Blog = ({blog}) => {
+
+    console.log(blog);
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+Blog.propTypes = {
+    blog: PropTypes.object
+}
+
+export default Blog;
