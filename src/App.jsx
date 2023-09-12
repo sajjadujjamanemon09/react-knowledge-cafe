15c0f397b55bd89e85050c80f9dca38c37574896
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-09-12 11:15:55 +0600

    six commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,31 @@ import Header from "./Components/Header/Header";
 
 function App() {
   const [bookmarks, setBookmarks] = useState([])
+  const [readingTime, setReadingTime] = useState(0)
+
+
   const handleAddToBookmark = blog => {
     const newBookmarks = [...bookmarks, blog]
     setBookmarks(newBookmarks)
   }
+
+  const handleMarkAsRead = (id, time) =>{
+    const newReadingTime = (readingTime + time)
+    setReadingTime(newReadingTime)
+
+    // remove the read blog from bookmark
+    // console.log('remove bookmark', id);
+
+    const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id)
+    setBookmarks(remainingBookmarks)
+  }
+
   return (
     <>
       <Header></Header>
       <div className="md:flex max-w-6xl mx-auto">
-        <Blogs handleAddToBookmark ={handleAddToBookmark}></Blogs>
-        <Bookmarks bookmarks={bookmarks}></Bookmarks>
+        <Blogs handleAddToBookmark ={handleAddToBookmark} handleMarkAsRead = {handleMarkAsRead}></Blogs>
+        <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
       </div>
     </>
   );
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog, handleAddToBookmark}) => {
+const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
 
-    const {title, cover, author, author_img, posted_date, hashtags, reading_time} = blog;
+    const {id, title, cover, author, author_img, posted_date, hashtags, reading_time} = blog;
     return (
-        <div className='mb-20'>
+        <div className='mb-20  space-y-4'>
             <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
             <div className='flex justify-between mb-4'>
                 <div className='flex'>
@@ -27,6 +27,9 @@ const Blog = ({blog, handleAddToBookmark}) => {
                 hashtags.map((hash, idx) => <span key={idx}><a href="">  #{hash}</a></span>)
                 }
             </p>
+            <button 
+            onClick={()=> handleMarkAsRead(id, reading_time) }
+            className='text-purple-700 font-bold underline'>Mark As Read</button>
         </div>
     );
 };
@@ -34,7 +37,8 @@ const Blog = ({blog, handleAddToBookmark}) => {
 
 Blog.propTypes = {
     blog: PropTypes.object,
-    handleAddToBookmark: PropTypes.func
+    handleAddToBookmark: PropTypes.func,
+    handleMarkAsRead: PropTypes.func
 }
 
 export default Blog;
